cb3551465e893cf049268c23df0da7e0999509cb
Sergei <sergei.avramenko199@yandex.ru>
2023-02-01 23:59:27 +0300

    NavLinklesson20

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import n from './NavBar.module.css';
+import {NavLink} from "react-router-dom";
 
 const NavBar = (props) => {
     return (
     <div className={n.nav}> 
       <div className={n.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" className = { navData => navData.isActive ? n.active : n.item }>Profile</NavLink>
       </div>
       <div className={ `${n.item} ${n.active} ` }>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" className = { navData => navData.isActive ? n.active : n.item }>Messages</NavLink>
       </div>
-      <div className={ `${n.item} ${n.active} ` }>
-        <a href="/news">News</a>
-      </div>
-      <div className={n.item}>
-        <a href="/music">Music</a>
+      <div className={n.item} >
+        <NavLink to="/news" className = { navData => navData.isActive ? n.active : n.item }>News</NavLink>
       </div>
       <div className={n.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/music" className = { navData => navData.isActive ? n.active : n.item }>Music</NavLink>
+      </div>
+      <div className={n.item}>
+        <NavLink to="/settings" className = { navData => navData.isActive ? n.active : n.item }>Settings</NavLink>
       </div>
    </div >
     );
