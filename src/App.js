fb353ef33ab548cf16e27b292c95ca4f93de5be1
Sergei <sergei.avramenko199@yandex.ru>
2023-02-01 13:27:51 +0300

    lesson 15

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Component/Header';
-import NavBar from './Component/NavBar';
-import Profile from './Component/Profile';
+import Header from './Component/Header/Header';
+import NavBar from './Component/NavBar/NavBar';
+import Profile from './Component/Profile/Profile';
 
 function App() {
   return (
---
+++ src/Component/Profile/MyPosts/MyPost.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import p from './MyPost.module.css';
+
+const MyPost = () => {
+    return (
+        <div className={p.content}>
+            <div>
+                <img src="https://images.unsplash.com/photo-1669993427076-3d9acc119413?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div >
+                My post
+                <div >
+                    New post </div>
+                <div className={p.posts}>
+                    <div className={p.item}>Post 1</div>
+                    <div className={p.item}>Post 2</div>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+
+    );
+}
+export default MyPost;
