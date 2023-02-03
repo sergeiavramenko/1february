7044be8da4389c926d469f7bf17729607ccae6a3
Sergei <sergei.avramenko199@yandex.ru>
2023-02-03 15:09:24 +0300

    leddom31

--- src/Component/Profile/MyPosts/MyPosts.jsx
+++ src/Component/Profile/MyPosts/MyPosts.jsx
@@ -4,26 +4,27 @@ import Post from "./Post/Post";
 
 
 const MyPosts = (props) => {
-    /*let postsData = [
-        {name:"Dimmamsamdsa", id:"1",likesCount:112},
-        {name:"Andr", id:"2",likesCount:90},
-        {name:"Andr", id:"2",likesCount:30},
-        {name:"Andr", id:"2",likesCount:1},
-        {name:"Andr", id:"2",likesCount:18},
-    ];*/
+
 
     let PostsElement = props.posts.map( ex => <Post message={ex.name} likesCount={ex.likesCount} /> )
-    /*const name = props.name*/;
+
+    let newPostElement = React.createRef();
+    let addPost = () => {
+    return (
+        alert(newPostElement.current.value)
+    );
+}
+
     return (
 
         <div className={p.postsBlock}>
            <h3>My post </h3>
             <div> console.log({props.a})
                 <div>
-                    <textarea> </textarea>
+                    <textarea ref={newPostElement}> </textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost }>Add post</button>
                 </div>
 
 
